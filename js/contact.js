6014dfb1fb1587377f990b7e63ded77fc486e10b
Michael River Nilsson <99018848+RiverMichael@users.noreply.github.com>
2023-05-16 21:37:13 +0200

    CSS for contact form and blogpost page

--- js/contact.js
+++ js/contact.js
@@ -10,20 +10,19 @@ const subject = document.querySelector("#subject");
 const subjectError = document.querySelector("#subject-error");
 const message = document.querySelector("#message");
 const messageError = document.querySelector("#message-error");
-const contactValidationContainer = document.querySelector(".contact-validation");
+const contactValidation = document.querySelector(".contact-validation");
 const modalOverlay = document.querySelector(".modal-overlay");
 const validationCloseButton = document.querySelector(".icon-close");
 
 form.addEventListener("submit", validateForm);
+modalOverlay.addEventListener("click", () => closeModal(contactValidation, modalOverlay));
+validationCloseButton.addEventListener("click", () => closeModal(contactValidation, modalOverlay));
 
-modalOverlay.addEventListener("click", () => closeModal(contactValidationContainer, modalOverlay));
-validationCloseButton.addEventListener("click", () => closeModal(contactValidationContainer, modalOverlay));
-
+// Validate Contact Form
 function validateForm(event) {
     event.preventDefault();
-
     if (validateLength(name, name.value, 5, nameError) && validateEmail(email, email.value, emailError) && validateLength(subject, subject.value, 15, subjectError) && validateLength(message, message.value, 25, messageError)) {
         form.reset();
-        openModal(contactValidationContainer, modalOverlay);
+        openModal(contactValidation, modalOverlay);
     }
 };
